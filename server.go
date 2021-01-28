7c0c6aafbed7eb59b5d88d18313dbd073423159f
HenryBrown0 <me@henrybrown0.com>
2021-01-28 14:49:45 +0000

    Add DFT_SECRET requirement to server

--- server.go
+++ server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +13,10 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	if _, isPresent := os.LookupEnv("DFT_SECRET"); isPresent == false {
+		log.Panic("No DFT_SECRET provided. Exiting...")
+	}
+
 	http.HandleFunc("/", mainHandler)
 
 	fmt.Println("Listening on port 5050...")
