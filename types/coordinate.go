4e50891b22a8c3a280d9280b18c406c388a7df85
HenryBrown0 <me@henrybrown0.com>
2021-02-17 10:07:02 +0000

    Fix coordinate typo

--- types/coordinate.go
+++ types/coordinate.go
@@ -1,6 +1,6 @@
 package types
 
-// Coordinate contains a single corrdinate with longitude and latitude
+// Coordinate contains a single coordinate with longitude and latitude
 type Coordinate struct {
 	Longitude float32
 	Latitude  float32
