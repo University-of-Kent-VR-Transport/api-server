2a11d717278a1c10bca18478d2bca1d36321ed61
HenryBrown0 <me@henrybrown0.com>
2021-03-16 16:34:57 +0000

    Add static assets

--- server.go
+++ server.go
@@ -11,9 +11,15 @@ import (
 func main() {
 	if _, isPresent := os.LookupEnv("MAPBOX_TOKEN"); isPresent == false {
 		log.Panic("No MAPBOX_TOKEN provided. Exiting...")
-}
+	}
 
 	router := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	router.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	router.Handle("/docs", http.RedirectHandler("https://github.com/University-of-Kent-VR-Transport/api-server/tree/master/docs", 301))
+	router.Handle("/download", http.RedirectHandler("https://github.com/University-of-Kent-VR-Transport/vr-client/releases", 301))
 
 	router.HandleFunc("/", handlers.IndexHandler)
 
---
+++ static/scripts/web-worker.js
@@ -0,0 +1,60 @@
+const uriPath = "/api/get-bus-locations"
+
+// onmessage receives the bounds{} and currentBuses[] on the map. It then gets
+// new buses within the bounds and returns the buses to; add, update, and remove
+onmessage = (event) => {
+	const {
+		bounds,
+		currentBuses,
+	} = event.data;
+
+  // fetch bus locations
+	getBusLocations(bounds)
+		.then((nextBuses) => {
+			// sort buses into; add, update, remove
+			postMessage({ buses: sortBuses(currentBuses, nextBuses) })
+		})
+		.catch((error) => postMessage({ error }))
+}
+
+const getBusLocations = (bounds) => {
+	const requestUrl =
+		`${uriPath}?topLeft=${bounds._sw.lng},${bounds._ne.lat}&bottomRight=${bounds._ne.lng},${bounds._sw.lat}`;
+
+	return new Promise((resolve, reject) => {
+		fetch(requestUrl)
+			.then(response => response.json())
+			.then(unformattedBuses => resolve(unformattedBuses))
+			.catch(error => reject(error))
+	})
+}
+
+const sortBuses = (currentBuses, updatedBuses) => {
+	busesToAdd = [];
+	busesToUpdate = [];
+	busesToRemove = currentBuses;
+
+	for (
+		let updatedBusesIndex = 0;
+		updatedBusesIndex < updatedBuses.length;
+		updatedBusesIndex++
+	) {
+		const updatedBus = updatedBuses[updatedBusesIndex];
+
+		busToRemoveIndex = busesToRemove.findIndex(bus => bus.ID == updatedBus.ID);
+		if (busToRemoveIndex == -1) {
+			// new bus
+			busesToAdd.push(updatedBus);
+		} else {
+			// update bus
+			busesToUpdate.push(updatedBus);
+			busesToRemove.slice(busToRemoveIndex, 1);
+		}
+	}
+
+	return {
+		add: busesToAdd,
+		update: busesToUpdate,
+		remove: busesToRemove,
+	}
+}
